42850f100738f33050597c2f8f258be5bedbb9db
cayman444 <artursavinX@yandex.ru>
2025-11-15 02:06:23 +0300

    feat: change visible price range

--- client/src/features/advertisements/components/AdFilterPrice.tsx
+++ client/src/features/advertisements/components/AdFilterPrice.tsx
@@ -1,4 +1,4 @@
-import { Form, InputNumber } from 'antd';
+import { InputNumber } from 'antd';
 import { memo } from 'react';
 import { useFilterPrice } from '../hooks';
 import type { AdChangeFilterParams, AdFilterPriceType } from '../model';
@@ -13,40 +13,37 @@ const AdFilterPriceComponent = (filterPriceProps: AdFilterPriceProps) => {
 
   return (
     <div className="flex gap-2 flex-wrap">
-      <div className="flex items-center gap-2">
-        <Form.Item
-          label={<span className="text-base">Минимальная цена</span>}
-          htmlFor="min-price"
-          className="mb-0!"
-        >
+      <div className="flex-1 flex items-center gap-2 flex-wrap">
+        <label htmlFor="min-price" className="text-nowrap">
+          Минимальная цена:
+        </label>
+        <div className="flex-1">
           <InputNumber
             id="min-price"
             suffix="₽"
             min={0}
             value={localMinPrice}
             onChange={(value) => setLocalMinPrice(value)}
-            className="w-30!"
+            className="w-full! min-w-30!"
             placeholder="мин"
           />
-        </Form.Item>
+        </div>
       </div>
-      <div className="flex items-center gap-2">
-        <Form.Item
-          label={<span className="text-base">Максимальная цена</span>}
-          htmlFor="max-price"
-          className="mb-0!"
-          style={{ fontSize: 16 }}
-        >
+      <div className="flex-1 flex items-center gap-2 flex-wrap">
+        <label htmlFor="max-price" className="mb-0!">
+          Максимальная цена:
+        </label>
+        <div className="flex-1">
           <InputNumber
             id="max-price"
             suffix="₽"
             min={0}
             value={localMaxPrice}
             onChange={(value) => setLocalMaxPrice(value)}
-            className="w-30!"
+            className="w-full! min-w-30!"
             placeholder="макс"
           />
-        </Form.Item>
+        </div>
       </div>
     </div>
   );
